830e622b744d12fa7769ceac7b583cf862678c8a
andieljm <90933472+andieljm@users.noreply.github.com>
2022-06-08 15:47:41 -0600

    tarea 2

---
+++ src/main/java/tarea2web/demo/Tarea2webApplication.java
@@ -0,0 +1,13 @@
+package tarea2web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2webApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tarea2web/demo/Tarea2webApplicationTests.java
@@ -0,0 +1,13 @@
+package tarea2web.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2webApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
